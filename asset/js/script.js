8383a6e302ca40d13aaefae25f25ba029c885741
davistheweb <smartkelvin775@gmail.com>
2024-01-31 15:46:30 +0100

    new changes

--- asset/js/script.js
+++ asset/js/script.js
@@ -103,6 +103,27 @@ watchVideo.addEventListener('click', () => {
 });
 
   
+let swiperCards = new Swiper(".cheese-content", {
+  loop: true,
+  spaceBetween: 40,
+  grabCursor: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
   //scroll reveal
 
   ScrollReveal({   
